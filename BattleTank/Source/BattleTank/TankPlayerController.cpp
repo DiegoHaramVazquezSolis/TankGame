6603f618d6113f8af2e490b014e03725cfd1b7e7
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2020-05-27 12:35:36 -0500

    205 Override of BeginPlay

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,19 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    ATank* ControlledTank = GetControlledTank();
+    if (ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Current controlled tank is: %s"), *ControlledTank->GetName());
+    } else {
+        UE_LOG(LogTemp, Error, TEXT("Current controlled can not be determined"));
+    }
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
